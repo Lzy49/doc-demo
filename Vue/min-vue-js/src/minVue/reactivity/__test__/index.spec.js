8bd90dcd4145a5b4b6ad00c5a4a3614fcb81e4cf
lzy <362018513@qq.com>
2022-01-11 15:27:33 +0800

    增加一个测试

--- Vue/min-vue-js/src/minVue/reactivity/__test__/index.spec.js
+++ Vue/min-vue-js/src/minVue/reactivity/__test__/index.spec.js
@@ -15,4 +15,19 @@ describe('reactive', () => {
     store.num2 = 100;
     expect(store.num2).toBe(100);
   });
+  it('reactive 双 effect 测试', () => {
+    const ret = reactive({
+      num: 0
+    });
+    let val;
+    let val2;
+    effect(() => {
+      val = ret.num;
+    });
+    effect(() => {
+      val2 = ret.num;
+    });
+    expect(val).toBe(0);
+    expect(val2).toBe(0);
+  });
 });
