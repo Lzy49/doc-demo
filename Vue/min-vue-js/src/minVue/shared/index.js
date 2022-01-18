7d05ce65beb928eccd3ae7494140f6f734571629
lzy <362018513@qq.com>
2022-01-18 15:44:48 +0800

    工具

--- Vue/min-vue-js/src/minVue/shared/index.js
+++ Vue/min-vue-js/src/minVue/shared/index.js
@@ -1,1 +1,3 @@
 export const isObject = (obj) => typeof obj === 'object' && obj !== null;
+export const isFunction = (v) => typeof v === 'function';
+export const isString = (v) => typeof v === 'string';
